934aa5fd0c66415174282f4a09ae080aef958683
Grace Wang <wangxiaotian2012@gmail.com>
2022-05-03 04:00:52 +0000

    Ex4: where method

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,37 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+whistler = Store.create(
+  name: "Whistler",
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where("mens_apparel = true")
+
+@mens_stores.each { |store|
+  puts store.name
+  puts store.annual_revenue
+}
+
+@women_less1M_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@women_less1M_stores.each { |store|
+  puts store.name
+  puts store.annual_revenue
+}
