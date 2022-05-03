1c74ed724e57f8780b7efc02558d28d4e290c1c4
Grace Wang <wangxiaotian2012@gmail.com>
2022-05-03 05:58:40 +0000

    Ex7: Add validation for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a new store name:"
+store_name = gets.chomp
+
+new_store = Store.create(
+  name: store_name,
+)
+
+unless new_store.errors.empty?
+new_store.errors.each { |error|
+  puts "Error: #{error.full_message}"
+}
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  validate :carries_apparel
+
+  def carries_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:carries_nothing, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
 end
