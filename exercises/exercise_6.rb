0d36a9a8250afa131e27fef87508cd65e98aa3a3
Grace Wang <wangxiaotian2012@gmail.com>
2022-05-03 04:27:01 +0000

    Ex6: accociations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Marry", last_name: "Poppins", hourly_rate: 67)
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Alice", last_name: "Wonder", hourly_rate: 66)
+@store2.employees.create(first_name: "Mad", last_name: "Hatter", hourly_rate: 68)
+@store2.employees.create(first_name: "Queen", last_name: "Hearts", hourly_rate: 80)
